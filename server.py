e731b6e9485fa83576d25224ca2186b95c00123d
Ksenia-Evin <ksu_ryaz@mail.ru>
2020-07-09 22:40:07 +0300

    init

--- server.py
+++ server.py
@@ -1,24 +1,22 @@
 import os
 import sentry_sdk
 
-from bottle import Bottle, request
+from bottle import request, route, run
 from sentry_sdk.integrations.bottle import BottleIntegration
 
 sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegration()])
 
-app = Bottle()
-
-@app.route('/')  
+@route('/')  
 def index():  
     return 'welcome!'
 
-@app.route('/success')  
+@route('/success')  
 def index():  
     return 'success!'
 
-@app.route('/fail')  
+@route('/fail')  
 def error():
     raise RuntimeError("There is an error!")
     return
 
-app.run(host="localhost", port=8080)
+run(host="localhost", port=8080)
