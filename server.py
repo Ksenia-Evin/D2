c911b7d64cfaf80117d9807e4a7d38f38f31c0c4
Ksenia-Evin <ksu_ryaz@mail.ru>
2020-07-09 22:59:41 +0300

    init

--- server.py
+++ server.py
@@ -4,19 +4,30 @@ import sentry_sdk
 from bottle import route, run
 from sentry_sdk.integrations.bottle import BottleIntegration
 
-sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegration()])
+sentry_sdk.init(
+    dsn="https://3864b7b5f7a04a458b434152ccd01ea1@o417300.ingest.sentry.io/5316483", 
+    integrations=[BottleIntegration()]
+)
 
-@route('/')  
+@route("/")  
 def index():  
     return 'welcome!'
 
-@route('/success')  
+@route("/success")  
 def index():  
     return 'success!'
 
-@route('/fail')  
+@route("/fail")  
 def error():
     raise RuntimeError("There is an error!")
     return
 
-run(host="localhost", port=8080)
+if os.environ.get("APP_LOCATION") == "heroku":
+    run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    run(host="localhost", port=8080, debug=True)
